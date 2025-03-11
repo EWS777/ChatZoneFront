7d1f2006dc700402fa18e00e2b9ce12c553fc529
EWS777 <egor.vasilenko2004@gmail.com>
2025-03-11 22:49:36 +0100

    init commit

---
+++ src/app/authorization/authorization.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from '@angular/core';
+
+@Component({
+  selector: 'authorization-app', //the name of Component, also should be as unique
+  templateUrl: 'authorization.component.html',
+  standalone: true,    //?
+  styleUrl: 'authorization.component.css'
+})
+
+export class AuthorizationComponent{}
