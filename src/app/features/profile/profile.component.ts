68c4502be4e85f3c3fc1e4c828798074b37366b4
EWS777 <egor.vasilenko2004@gmail.com>
2025-06-22 18:24:27 +0200

    feat: implement update profile logic into component and add navigation for delete profile and update password

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {ProfileService} from './profile.service';
 import {FormsModule} from '@angular/forms';
 import {Profile} from './profile';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -14,13 +15,37 @@ import {Profile} from './profile';
 })
 export class ProfileComponent implements OnInit{
   profileService = inject(ProfileService)
+  router = inject(Router)
 
   profile!: Profile;
+  oldUsername!: string;
 
   ngOnInit() {
     this.profileService.getProfile()
       .subscribe(value => {
         this.profile = value
+        this.oldUsername = value.username
       })
+  }
+
+  onClickUpdate(){
+    this.profileService.updateProfile(this.oldUsername, this.profile)
+      .subscribe({
+        next: value => {
+          this.profile = value
+          this.oldUsername = value.username
+        },
+        error: err => {
+          console.error('Update profile failed!', err)
+        }
+      })
+  }
+
+  onClickDeleteProfile(){
+    this.router.navigate([`delete-profile`])
+  }
+
+  onClickUpdatePassword(){
+    this.router.navigate([`update-password`])
   }
 }
