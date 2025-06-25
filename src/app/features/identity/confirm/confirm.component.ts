fff06975a7ae2a35754e0757a5e7cfff6fabd806
EWS777 <egor.vasilenko2004@gmail.com>
2025-06-26 01:20:36 +0200

    fix&feat: modified confirm request and added reconfirm logic into service, added confirm and reconfirm logic into component

--- src/app/features/identity/confirm/confirm.component.ts
+++ src/app/features/identity/confirm/confirm.component.ts
@@ -1,28 +1,50 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ConfirmService} from './confirm.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-confirm',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './confirm.component.html',
   styleUrl: './confirm.component.css'
 })
 export class ConfirmComponent implements OnInit{
-
   router = inject(Router)
   route = inject(ActivatedRoute)
-
   confirmService = inject(ConfirmService)
+
+  isConfirmed = signal<boolean | null>(null)
+  email: string | null = null
 
   ngOnInit(): void {
     const token = this.route.snapshot.queryParamMap.get('link')
-
     if (token){
-      this.confirmService.confirm(token).subscribe()
+      this.confirmService.confirm(token).subscribe({
+        next: () => {
+          this.isConfirmed.set(true)
+          this.router.navigate([''])
+        },
+        error: err => {
+          this.isConfirmed.set(false)
+          console.log('Have not confirmed!', err)
+        }
+      })
     }else {
       console.log('Token is not exists!')
     }
-    }
+  }
 
+  reconfirm(){
+    this.confirmService.reconfirm(this.email!).subscribe({
+      next: () => {
+        this.router.navigate([''])
+      },
+      error: err => {
+        console.log('Has not confirm again!', err)
+      }
+    })
+  }
 }
--- src/app/features/identity/confirm/confirm.service.ts
+++ src/app/features/identity/confirm/confirm.service.ts
@@ -6,10 +6,15 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ConfirmService {
   http = inject(HttpClient)
-  url = 'https://localhost:7212/registration/confirm'
+  url = 'https://localhost:7212/registration'
 
   confirm(link: string){
-    return this.http.post(`${this.url}?token=${link}`, null
-    )
+    return this.http.post(`${this.url}/confirm?token=${link}`, null,{
+      withCredentials: true
+      })
+  }
+
+  reconfirm(email: string){
+    return this.http.post(`${this.url}/reconfirm?email=${email}`, {})
   }
 }
