e86a60e1af02f58968248d28b3028c597a513cf6
EWS777 <egor.vasilenko2004@gmail.com>
2025-07-13 00:53:35 +0200

    feat: add filter to find person into main component

---
+++ src/app/features/main/find-person.ts
@@ -0,0 +1,9 @@
+export interface FindPerson {
+  connectionId: string
+  themeList: number | null,
+  country: number | null,
+  city: number | null,
+  age: number | null,
+  gender: number | null,
+  lang: number | null,
+}
--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,11 +1,23 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {ProfileService} from '../profile/profile.service';
 import {Router} from '@angular/router';
 import {AuthorizationService} from '../identity/authorization/authorization.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CountryList} from '../profile/filter/enums/country-list';
+import {AgeList} from '../profile/filter/enums/age-list';
+import {CityList} from '../profile/filter/enums/city-list';
+import {LangList} from '../profile/filter/enums/lang-list';
+import {ThemeList} from '../profile/filter/enums/theme-list';
+import {GenderList} from '../profile/filter/enums/gender-list';
+import {SignalRService} from '../../chat/signalR.service';
+import {FindPerson} from './find-person';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './main.component.html',
   standalone: true,
   styleUrl: './main.component.css'
@@ -13,9 +25,45 @@ import {AuthorizationService} from '../identity/authorization/authorization.serv
 export class MainComponent implements OnInit{
   router = inject(Router)
   authService = inject(AuthorizationService)
+  signalRService = inject(SignalRService)
   profile = inject(ProfileService)
 
   username: string | null = null;
+  isFilterActivated = signal<boolean>(false)
+
+  filter: FindPerson = {
+    connectionId: '',
+    themeList: null,
+    country: null,
+    city: null,
+    age: null,
+    gender: null,
+    lang: null
+  }
+
+  countryList = Object.keys(CountryList)
+    .filter(k => isNaN(Number(k)))
+    .map(name => ({ label: name, value: (CountryList as any)[name] as number }));
+
+  ageList = Object.keys(AgeList)
+    .filter(k => isNaN(Number(k)))
+    .map(name => ({ label: name, value: (AgeList as any)[name] as number }));
+
+  cityList = Object.keys(CityList)
+    .filter(k => isNaN(Number(k)))
+    .map(name => ({ label: name, value: (CityList as any)[name] as number }));
+
+  langList = Object.keys(LangList)
+    .filter(k => isNaN(Number(k)))
+    .map(name => ({ label: name, value: (LangList as any)[name] as number }));
+
+  themeList = Object.keys(ThemeList)
+    .filter(k => isNaN(Number(k)))
+    .map(name => ({ label: name, value: (ThemeList as any)[name] as number }));
+
+  genderList = Object.keys(GenderList)
+    .filter(k => isNaN(Number(k)))
+    .map(name => ({ label: name, value: (GenderList as any)[name] as number }));
 
 
   ngOnInit(){
@@ -34,6 +82,21 @@ export class MainComponent implements OnInit{
 
   onClickProfile(){
     this.router.navigate([`profile`])
+  }
+
+  activateFilter(){
+    this.isFilterActivated.set(!this.isFilterActivated())
+  }
+
+  findPerson(){
+    this.filter.connectionId = this.signalRService.connectionId
+
+    this.signalRService.findPerson(this.filter).subscribe({
+      next: () => { },
+      error: err => {
+        console.error('Error', err)
+      }
+    })
   }
 
   onClickLogout(){
