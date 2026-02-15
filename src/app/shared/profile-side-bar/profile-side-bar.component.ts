049180373a1f864cf95f603a98307a4f3f5b56fd
EWS777 <egor.vasilenko2004@gmail.com>
2026-02-15 19:01:03 +0100

    feat: implement small changes and css for profile side bar

--- src/app/shared/profile-side-bar/profile-side-bar.component.ts
+++ src/app/shared/profile-side-bar/profile-side-bar.component.ts
@@ -1,32 +1,33 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-profile-side-bar',
   imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './profile-side-bar.component.html',
   styleUrl: './profile-side-bar.component.css'
 })
 export class ProfileSideBarComponent {
+  router = inject(Router)
+  isDropdownOpen = false; // Состояние меню
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  get activePageName(): string {
+    const current = this.menuItems.find(item => this.router.url.includes(item.link));
+    return current ? current.name : 'Menu';
+  }
+
   menuItems = [
-    {
-      name: 'Profile',
-      link: '/profile'
-    },
-    {
-      name: 'Filter',
-      link: 'filter'
-    },
-    {
-      name: 'Quick messages',
-      link: 'quick-messages'
-    },
-    {
-      name: 'Blocked users',
-      link: 'blocked-users'
-    }
+    { name: 'Profile', link: '/profile'},
+    { name: 'Filter', link: 'filter'},
+    { name: 'Quick messages', link: 'quick-messages'},
+    { name: 'Blocked users', link: 'blocked-users'}
   ]
 }
