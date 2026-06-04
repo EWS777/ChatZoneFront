22547923e6c2769dbb473cfdaba7c339959e703f
EWS777 <egor.vasilenko2004@gmail.com>
2026-06-04 14:43:57 +0200

    feat: implement csrf interceptor

---
+++ src/app/core/interceptor/csrf.interceptor.ts
@@ -0,0 +1,20 @@
+﻿import {HttpInterceptorFn} from '@angular/common/http';
+
+export const csrfInterceptor: HttpInterceptorFn = (req, next) => {
+  const getCookie = (name: string) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift;
+    return null;
+  }
+
+  const xsrfToken = getCookie('XSRF-TOKEN');
+
+  if (xsrfToken && !['GET', 'HEAD', 'OPTIONS'].includes(req.method)){
+    req = req.clone({
+      headers: req.headers.set('X-XSRF-TOKEN', xsrfToken)
+    });
+  }
+
+  return next(req);
+}
