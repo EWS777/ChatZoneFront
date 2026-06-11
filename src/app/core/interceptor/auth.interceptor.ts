89444cf4deeb6d77eb38c6c4e58e160c8efbeb5f
EWS777 <egor.vasilenko2004@gmail.com>
2026-06-11 17:27:22 +0200

    feat(interceptor): prevent infinite refresh Token request

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -1,22 +1,43 @@
 ﻿import {HttpInterceptorFn} from '@angular/common/http';
 import {inject} from '@angular/core';
 import {AuthorizationService} from '../../features/identity/authorization/authorization.service';
-import {catchError, switchMap, throwError} from 'rxjs';
+import {BehaviorSubject, catchError, filter, switchMap, take, throwError} from 'rxjs';
+
+let isRefreshing = false;
+const refreshTokenSubject = new BehaviorSubject<boolean | null>(null);
 
 export const authInterceptor: HttpInterceptorFn = (req, next) =>{
   const authService = inject(AuthorizationService)
 
-  const isRefreshUrl = req.url.endsWith('/refresh');
+  const isAuthUrl = req.url.includes('authentication/refresh') || req.url.includes('authentication/logout');
 
   return next(req).pipe(
     catchError(err => {
-      if (err.status === 401 && !isRefreshUrl){
+      if (err.status === 401 && !isAuthUrl){
+
+        if (isRefreshing){
+          return refreshTokenSubject.pipe(
+            filter(result => result !== null),
+            take(1),
+            switchMap(() => next(req))
+          )
+        }
+
+        isRefreshing = true
+        refreshTokenSubject.next(null)
+
         return authService.refreshToken().pipe(
-          switchMap(() => next(req)),
-          catchError((refreshErr) =>{
-            return throwError(() => refreshErr)
+          switchMap(() => {
+            isRefreshing = false;
+            refreshTokenSubject.next(true);
+            return next(req);
+          }),
+          catchError((refreshErr) => {
+            isRefreshing = false;
+            refreshTokenSubject.next(false);
+            return throwError(() => refreshErr);
           })
-        )
+        );
       }
       return throwError(()=> err)
     })
